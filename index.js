3a25872708643e34db61a9face4cfc5ce3ec0b9e
Anugrah K <anugrahkorembeth.ak@gmail.com>
2021-10-17 23:08:05 +0530

    added cmyk

--- index.js
+++ index.js
@@ -16,11 +16,23 @@ res.send('Welcome to paletty api');
 // convert RGB
 
 app.get('/convertRGB/:rgb',async(req,res)=>{
-const {rgb}=req.params;
+const rgb=req.params.rgb;
 try {
 
     var splitRGB = rgb.split(',');
- const response=convert.rgbToHSL(splitRGB[0],splitRGB[1],splitRGB[2]);
+ const response=convert.rgb(splitRGB[0],splitRGB[1],splitRGB[2]);
+    res.json(response);
+} catch (error) {
+    res.json(error);
+}
+
+});
+app.get('/convertHSL/:hsl',async(req,res)=>{
+const hsl=req.params.hsl;
+try {
+
+    var splitHSL = hsl.split(',');
+ const response=convert.hsl(splitHSL[0],splitHSL[1],splitHSL[2]);
     res.json(response);
 } catch (error) {
     res.json(error);
